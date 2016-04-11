817b46117b0c47f1bf767f720ef464c2da37b5ad
Ivan <ivan@britecore.com>
2016-04-11 02:05:45 -0500

    archivos fixtures, tests limpiados, package tmb

--- test/testsApi.js
+++ test/testsApi.js
@@ -1,99 +1,31 @@
-var assert = require('assert') //var assert = require('assert-diff')
-var fs = require('fs');
-var http = require('http');
-var fixtures = require('./fixtures');
-var nock = require('nock');
-var app = require('../app');
-var request = require('request-json');
-var client = request.createClient('http://localhost:3000/api/v1/');
-
+var assert = require('assert')
 describe('API', function(){
-  after(function (done) {
-    app.close();
-    done();
-  });
 
   it('obtener informacion general de un vehiculo', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_infracciones_adeudos.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=183YTP')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener infracciones de un vehiculo', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_infracciones_adeudos.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=183YTP')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener adeudos de un vehiculo', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_infracciones_adeudos.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=183YTP')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener error cuando no se encuentra la placa en padron', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_no_en_padron.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=183YTP')
-        .reply(200, html);
-      client.get('vehiculos/154DBH', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener estado de sin infracciones', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_sin_infracciones.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=183YTP')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener estado de no adeudos de tenencias de un vehiculo', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_sin_adeudos_tenencia.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=908WEG')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 
   it('obtener placa anterior de un vehículo', function(done) {
-    fs.readFile(__dirname + '/fixtures_html/vehiculo_placa_anterior.html', 'utf8', function(err, html){
-      nock('http://www.finanzas.df.gob.mx')
-        .get('/sma/detallePlaca.php?placa=685UYK')
-        .reply(200, html);
-      client.get('vehiculos/183YTP', function(err, res, body) {
-        assert.fail('Terminar test');
-        done();
-      });
-    });
+    assert.fail('');
   });
 });
