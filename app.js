7352527d67d4395baa6531c5e97efe7c4f27b571
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2016-05-31 19:03:32 -0500

    add logging

--- app.js
+++ app.js
@@ -9,40 +9,51 @@ URL_API = '/api/v1'
 app.disable('x-powered-by');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.get(URL_API + '/vehiculos/:placas', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  var placas = req.params.placas;
-  scraper.scrapeVehiculo(placas, function(vehiculo) {
-    res.send(vehiculo);
-  })
+app.get(URL_API + '/vehiculos/:placas', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    var placas = req.params.placas;
+    scraper.scrapeVehiculo(placas, function(vehiculo) {
+        Zalamero = require('zalamero');
+        logger = new Zalamero({
+            mongoUrl: 'mongodb://:@:/transito-cdmx-api',
+            app: 'transito-cdmx-api@46.101.228.138'
+        });
+        logger.log({
+            request: req,
+            object: vehiculo
+        });
+        res.send(vehiculo);
+    })
 });
 
-app.get(URL_API + '/verificentros', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  var placas = req.params.placas;
-  scraper.scrapeVehiculo(placas, function(vehiculo) {
-    res.send(vehiculo);
-  })
+app.get(URL_API + '/verificentros', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    var placas = req.params.placas;
+    scraper.scrapeVehiculo(placas, function(vehiculo) {
+        res.send(vehiculo);
+    })
 });
 
-app.get(URL_API + '/corralones', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send(corralones);
+app.get(URL_API + '/corralones', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send(corralones);
 });
 
-app.use(function(req, res){
-  res.status(404);
-  res.set('Content-Type', 'application/json');
-  res.send({error: 'No existe el endpoint'});
+app.use(function(req, res) {
+    res.status(404);
+    res.set('Content-Type', 'application/json');
+    res.send({
+        error: 'No existe el endpoint'
+    });
 });
 
-var server = app.listen(3000, function () {
-  console.log('API de Transito CDMX corriendo en puerto 3000');
+var server = app.listen(3000, function() {
+    console.log('API de Transito CDMX corriendo en puerto 3000');
 });
 
 module.exports = server;
